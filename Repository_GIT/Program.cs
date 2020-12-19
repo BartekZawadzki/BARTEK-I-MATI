0b40b5d7d72d42ae21a1e4aa559cd050d366a36d
Zawadiaka <58304441+BartekZawadzki@users.noreply.github.com>
2020-12-19 16:24:24 +0100

    Add files via upload

---
+++ Repository_GIT/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello Mati, its Bartek");
+        
+           
+        }
+        
+    }
+}
